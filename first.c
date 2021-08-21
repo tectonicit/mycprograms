5ad1eb87c6c295d07572b569cb7ee2632b3bb498
tec <yashodeepgaikwad@gmail.com>
2021-08-21 09:55:08 +0530

    firstprograms

---
+++ first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+    int num1,num2,r;
+    printf("Enter 2 numbers:");
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    r=num1+num2;
+    printf("Addition is : %d",r);
+
+
+}
